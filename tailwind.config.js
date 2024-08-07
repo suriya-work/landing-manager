7497749972a001a296b8528cc0e9b6f3c18793a9
suriya babaei <suriyalifework60@gmail.com>
2024-08-07 18:48:46 +0330

    add typography and forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,14 +13,26 @@ export default {
         purple: '#1e2130',
         purpleLight:'#685dd8',
         gray: '#25273a',
-        grayLight:'#cfcde4',
+        grayLight: '#cfcde4',
+        grayDark:'#2f3349',
+      },
+      backgroundImage: {
+        'gradient-to-r': 'linear-gradient(to right, #28c76f 0%, #5a4aff 47.92%, #ff3739 100%)',
+      },
+      textColor: {
+        'gradient-from': '#28c76f',
+        'gradient-via': '#5a4aff',
+        'gradient-to': '#ff3739',
       },
       
     },
-    backgroundImage:{},
+    // backgroundImage:{},
     container: {
       center: true,
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+  ],
 }
