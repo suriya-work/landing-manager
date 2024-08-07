64477a987a55e65c8fb6ba6bf9d526e6ec635418
suriya babaei <suriyalifework60@gmail.com>
2024-08-07 16:42:03 +0330

    add react-scroll.d.ts

---
+++ src/react-scroll.d.ts
@@ -0,0 +1,23 @@
+declare module 'react-scroll' {
+    import * as React from 'react';
+
+    export interface LinkProps {
+        // href: string;
+        spy?: boolean;
+        smooth?: boolean;
+        offset?: number;
+        duration?: number;
+        isDynamic?: boolean;
+        onSetActive?: () => void;
+        onSetInactive?: () => void;
+        ignoreCancelEvents?: boolean;
+        delay?: number;
+        href?: string;
+        containerId?: string;
+        activeClass?: string;
+        className?: string;
+        children?: React.ReactNode;
+    }
+
+    export class Link extends React.Component<LinkProps> {}
+}
