9855e90a7c339a089f2c8f987565567f1c470a2e
suriya babaei <suriyalifework60@gmail.com>
2024-08-07 18:49:04 +0330

    add App : components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
+import Features from "./components/features/Features";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
-import HeroSection from './components/hero-section/HeroSection';
 const App = () => {
   return (
-    <div className="min-h-screen grid grid-rows-[80px_1fr_auto] bg-bgColor">
+    <div className="min-h-screen grid grid-rows-[auto_1fr_auto] bg-bgColor">
       <Header />
-      {/* <HeroSection /> */}
-      <main>
-        HeroSection
+      <main >
+        <Features />
       </main>
       <Footer />
     </div>
