43097b05aa03f80225168055598df28d0103e2c7
suriya babaei <suriyalifework60@gmail.com>
2024-08-10 15:35:57 +0330

    add mainLayout

---
+++ src/layouts/MainLayouts.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
+const MainLayouts = () => {
+  return (
+    <div className="min-h-screen grid grid-rows-[auto_1fr_auto] bg-bgColor" id="#home">
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayouts;
