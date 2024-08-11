c70268fed508d7dada3bde7664c3263e58235d40
suriya babaei <suriyalifework60@gmail.com>
2024-08-11 19:06:49 +0330

    fixed MainLayout

--- src/layouts/MainLayouts.tsx
+++ src/layouts/MainLayouts.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
+
 const MainLayouts = () => {
+ 
+
   return (
-    <div className="min-h-screen grid grid-rows-[auto_1fr_auto] bg-bgColor" id="#home">
+    <div className="min-h-screen grid grid-rows-[auto_1fr_auto] bg-bgColor">
       <Header />
       <main>
         <Outlet />
