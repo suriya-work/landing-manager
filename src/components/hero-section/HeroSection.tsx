5c881e5c1f64f21362389c4829dc64d30151e48d
suriya babaei <suriyalifework60@gmail.com>
2024-08-11 19:06:36 +0330

    add image to heroSection

--- src/components/hero-section/HeroSection.tsx
+++ src/components/hero-section/HeroSection.tsx
@@ -1,6 +1,41 @@
+import { motion, useMotionValue, useTransform } from "framer-motion";
+import { useEffect } from "react";
 import CustomButton from "../custumbutton/CustomButoon";
 
 const HeroSection = () => {
+  
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+
+  const rotateX = useTransform(y, [-100, 100], [-15, 15]);
+  const rotateY = useTransform(x, [-100, 100], [-15, 15]);
+
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      const { clientX, clientY, currentTarget } = event;
+      const rect = (currentTarget as HTMLElement).getBoundingClientRect();
+      const offsetX = clientX - rect.left - rect.width / 2;
+      const offsetY = clientY - rect.top - rect.height / 2;
+
+      x.set(offsetX / 4); 
+      y.set(offsetY / 4); 
+    };
+
+    const imageElement = document.querySelector("#cta-image");
+
+    imageElement?.addEventListener(
+      "mousemove",
+      handleMouseMove as EventListener
+    );
+
+    return () => {
+      imageElement?.removeEventListener(
+        "mousemove",
+        handleMouseMove as EventListener
+      );
+    };
+  }, [x, y]);
+
   return (
     <div className="relative cursor-pointer">
       <div className="absolute inset-0 flex items-center justify-center">
@@ -22,6 +57,19 @@ const HeroSection = () => {
           دسترسی سریع
         </CustomButton>
       </div>
+      <motion.div
+        id="cta-image"
+        className="  absolute left-0 right-0 flex justify-center"
+        style={{ rotateX, rotateY }}
+      >
+        <img
+          src="/images/hero-dashboard-dark.png"
+          alt="Dashboard Image"
+          width={700}
+          height={700}
+          className="shadow-lg"
+        />
+      </motion.div>
     </div>
   );
 };
